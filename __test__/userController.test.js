b999467df650d9721e41dfbfc23745ffad249714
wenriqueaa <wenriqueaa@gmail.com>
2024-09-19 18:26:40 -0500

    update usercontroller.test.js

--- __test__/userController.test.js
+++ __test__/userController.test.js
@@ -6,18 +6,72 @@ const User = require('./../models/User')
 
 
 describe('User Controller testing', () =>{
-    beforeeach(async()=>{
+    const emailTest = "acme@acme.com"
+    const passwordTest = "Clave#654321"
+beforeEach(async()=>{
         await User.deleteMany()
     }, 10000 ) //Espera de 10 mil milisegundos
 
-    AfterAll(async()=>{
+    afterAll(async()=>{
         await moongose.connection.close()
     })
-    it("Este test registra un nuevo usuario en la base de datos",
-        async () => {const response = await request(app)
-        .post('/api/register')
-        .send({email:"",password:""})
-        expect()
+    // it("Este test registra un nuevo usuario en la base de datos",
+    //     async () => {
+    //         const response = await request(app)
+    //     .post('/api/register')
+    //     .send({email:emailTest,password:passwordTest})
+    //     expect()
+    //     }
+    // )
+    it("Debería loggear a un usuario con credenciales correctas",
+        async () => {
+            const hashedPassword = bcrypt.hashSync(passwordTest,10)
+            const user = await new User(
+                {email:emailTest
+                , password:hashedPassword}
+            )
+            user.save()
+            const response = await request(app)
+                .post('/api/login')
+                .send({email:user.email,password:passwordTest})
+            expect(response.statusCode).toBe(200)
+            expect(response.body).toHaveProperty('msg', `${user.email}, Wellcome app`)
+            expect(response.body).toHaveProperty('token')
+
         }
     )
+    it("No Debería loggear a un usuario con contraseña incorrecta",
+        async () => {
+            const hashedPassword = bcrypt.hashSync(passwordTest,10)
+            const user = await new User(
+                {email:emailTest
+                , password:hashedPassword}
+            )
+            user.save()
+            const response = await request(app)
+                .post('/api/login')
+                .send({email:user.email,password:user.password})
+            expect(response.statusCode).toBe(400)
+            expect(response.body).toHaveProperty('msg', 'incorrect password')
+            expect(response.body).toHaveProperty('ok', false)
+
+        }
+    )
+    it("retornar un error de servidor al hacer login",
+        async () => {
+            jest.spyOn(User, 'findOne').mockImplementationOnce(
+                () => {
+                    throw new Error('simulando error en base de datos') 
+                }
+            )
+            const response = await request(app)
+                .post('/api/login')
+                .send({email:emailTest,password:passwordTest})
+            expect(response.statusCode).toBe(500)
+            expect(response.body).toHaveProperty('msg', 'please contact to development team')
+            expect(response.body).toHaveProperty('ok', false)
+
+        }
+    )
+
 })
