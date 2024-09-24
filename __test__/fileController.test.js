38b3ba392012998f0988484db46ccb3f49289cf8
wenriqueaa <wenriqueaa@gmail.com>
2024-09-23 19:13:17 -0500

    update filecontroller.test.js

--- __test__/fileController.test.js
+++ __test__/fileController.test.js
@@ -7,8 +7,8 @@ const path = require('path')
 
 describe('File Controller Tests', () => {
     let tokenTest
-    const idExternal = new mongoose.Types.ObjectId()
-    console.info(idExternal)
+    // const idExternal = new mongoose.Types.ObjectId()
+    // console.info(idExternal)
     const filePath = path.resolve(__dirname, 'transporte-especial-de-pacientes-tep-corte-17-07-2024.csv')
     beforeAll(async () => {
         await File.deleteMany()
@@ -19,54 +19,74 @@ describe('File Controller Tests', () => {
         await mongoose.connection.close()
     })
 
-    it('Debería subir un archivo CSV y guardarlo en base de datos', async() => {
-         const filePath = path.resolve(__dirname, 'transporte-especial-de-pacientes-tep-corte-17-07-2024.csv')
-         console.log(filePath)
-         const response = await request(app)
-                         .post('/api/upload')
-                         .set('Authorization', `Bearer ${ tokenTest }`)
-                         .attach('file', filePath)
+    // it('Debería subir un archivo CSV y guardarlo en base de datos', async() => {
+    //      const filePath = path.resolve(__dirname, 'transporte-especial-de-pacientes-tep-corte-17-07-2024.csv')
+    //      console.log(filePath)
+    //      const response = await request(app)
+    //                      .post('/api/upload')
+    //                      .set('Authorization', `Bearer ${ tokenTest }`)
+    //                      .attach('file', filePath)
 
-        expect(response.statusCode).toBe(201)
-        expect(response.body).toHaveProperty('msg', 'File transporte-especial-de-pacientes-tep-corte-17-07-2024.csv created!!')
-    })
+    //     expect(response.statusCode).toBe(201)
+    //     expect(response.body).toHaveProperty('msg', 'File transporte-especial-de-pacientes-tep-corte-17-07-2024.csv created!!')
+    // })
 
-    it('No debería subir un file si no se adjunta', async() => {
-        const response = await request(app)
-                        .post('/api/upload')
-                        .set('Authorization', `Bearer ${ tokenTest }`)
+    // it('No debería subir un file si no se adjunta', async() => {
+    //     const response = await request(app)
+    //                     .post('/api/upload')
+    //                     .set('Authorization', `Bearer ${ tokenTest }`)
 
-        expect(response.statusCode).toBe(400)
-    })
+    //     expect(response.statusCode).toBe(400)
+    // })
 
     it('Debería traer un file si se llama por ID', async() => {
+
         const csvFile = await new File({
-             name:idExternal+'.csv'
+            name:`prueba_by_ID.csv`
              , data:[]
          }).save()
         //  console.info('csvFile ', csvFile._id)
-         const routeApi = '/api/file/'+csvFile._id
-         console.info('route', routeApi)
+         const routeApi = `/api/file/${csvFile._id}`
+         console.info('route', `${routeApi}`)
+         console.info('csvFile name', csvFile._id)
          console.info('csvFile name', csvFile.name)
         const response = await request(app)
-        .get('/api/file/'+csvFile._id)
+        .get(`${routeApi}`)
         .set('Authorization', `Bearer ${tokenTest}`)
-        console.info('response.statusCode', response.statusCode)
-        console.info('response', response)
+        console.info('response.statusCode', response.statusCode, csvFile._id)
+        // console.info('response', response)
 
         // expect(response.statusCode).toBe(200)
       
     })
-    it('Error file si se llama por ID que no existe', async() => {
-         const response = await request(app)
-        .get(`/api/file/${idExternal}`)
-        //  .get(`api/file/66ee164d098ba945498d8dc4`)
-         .set('Authorization', `Bearer ${tokenTest}`)
-        //  .set('params', `id="66ee164d098ba945498d8dc4"`)
-//         console.info('Debería traer un file ', response)
-         expect(response.statusCode).toBe(404)
+    // it('Error file si se llama por ID que no existe', async() => {
+    //      const response = await request(app)
+    //     .get(`/api/file/${idExternal}`)
+    //      .set('Authorization', `Bearer ${tokenTest}`)
+    //      expect(response.statusCode).toBe(404)
+      
+    // })
+    it('Debería borrar un file si se llama por ID', async() => {
+        // se genera una archivo        
+        const csvFile = await new File({
+            name:`delete_by_ID.csv`
+             , data:[]
+         }).save()
+         const idDelete = csvFile._id
+        //se invoca el api para borrar
+         const routeApi = `/api/delete-file/${idDelete}`
+        const response = await request(app)
+        .delete(`${routeApi}`)
+        .set('Authorization', `Bearer ${tokenTest}`)
+        console.info('response.statusCode', response.statusCode)
+        console.info('routeApi', routeApi )
+        console.info('idDelete', idDelete)
+        console.info('response.msg ok', response.msg, response.ok)
+
+        expect(response.statusCode).toBe(200)
       
     })
+
 })
 
 
